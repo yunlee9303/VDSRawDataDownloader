fc01e7102496a6c8c6368fb8a6549dbfd85ec87c
yunlee9303 <yunlee9303@gmail.com>
2023-07-17 16:03:20 +0900

    chore: add annotation for using this script

--- VDSRawDataDownloader.py
+++ VDSRawDataDownloader.py
@@ -6,6 +6,10 @@ import os
 import platform
 from PyQt5.QtWidgets import QApplication, QFileDialog
 
+# Using
+# 1. python VDSRawDataDownloader.py 202303(년+월) : 해당 년/월의 모든 데이터 검색 / 다운로드
+# 2. python VDSRawDataDownloader.py 20230304 20230307 : 해당 20230304 ~ 20230307까지의 데이터 검색 / 다운로드
+
 def search_data(date):
     url = 'http://data.ex.co.kr/portal/fdwn/search'
     user_agent = "Mozilla/5.0 ({}; {}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36".format(
